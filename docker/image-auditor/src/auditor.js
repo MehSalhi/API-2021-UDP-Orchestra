d54967c0b845d0951fdb6b9f09ede26ac3016cbc
mehdi <mehdi.salhi@heig-vd.ch>
2022-01-20 11:01:14 +0100

    protocol

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,60 +1,10 @@
-// Importe le module chance
-var Chance = require('chance');
-var chance = new Chance();
+// se connecte au broadcast
 
-// Importe le module express
-var express = require('express');
-var app = new express();
+// crée ou met à jour un tableau contenant le son de chaque musicien actif
+// depuis les 5 dernières seconds
 
-// Importe le module random-words-slugs
-var randomWords = require('random-word-slugs');
+// un timer par musicien qui est réinitialisé à chaque fois qu'un son est
+// recu de ce musicien. Si ce timer atteint 0, on supprime le musicien
 
-// execute le code à chaque requête "GET /"
-app.get('/', function(req, res) {
-    console.log("GET / received");
-    res.send(generateAnimals());
-});
-
-// execute le code à chaque requête "GET /test"
-app.get('/test', function(req, res) {
-    res.send("You tried to access /test.");
-});
-
-// Met l'application en mode écoute sur le port 3000 et execute la fonction
-// "function() à chaque nouvelle connexion
-app.listen(3000, function() {
-    console.log("Accepting HTTP request on port 3000");
-});
-
-// Fonction qui génère une String sous forme de liste d'animaux
-function generateAnimals() {
-    var animals = [];
-    //var animalsList = "Welcome to HEIG, these animals are currently enrolled at our school: \n";
-    var numberOfAnimals = chance.integer({
-        min: 5,
-        max: 10
-    });
-    console.log(numberOfAnimals);
-    for(var i = 0; i < numberOfAnimals; ++i) {
-        var gender = chance.gender();
-        var species = chance.animal();
-        var birthYear = chance.year({
-            min: 1800,
-            max: 2021,
-        });
-        var firstName = chance.first({ gender: gender });
-        var lastName = chance.last();
-        var word = randomWords.generateSlug(4, {format: "title" });
-
-        animals.push({
-            firstname: firstName,
-            lastname: lastName,
-            gend: gender,
-            specie: species,
-            birthyear: birthYear,
-            favouriteWords: word
-        });
-    };
-    console.log(animals);
-    return animals;
-}
+// il se met en écoute sur un port en attente de client
+// il envoie son tableau à chaque client connectés
---
+++ docker/orchestra-protocol.js
@@ -0,0 +1,6 @@
+/*
+ * Default broadcast adress and port
+ */
+
+exports.PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
+exports.PROTOCOL_PORT = 9907;
