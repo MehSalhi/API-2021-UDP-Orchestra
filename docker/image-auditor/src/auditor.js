eece3b807d44eefe15bb70a55badcb947c903a0a
Meh <mehdi.k.salhi@protonmail.com>
2022-01-31 17:34:28 +0100

    [feat] auditor

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -22,6 +22,7 @@ const {PROTOCOL_PORT, AUDITOR_PORT} = require("../../orchestra-protocol");
 
 // tableau de musiciens
 var musicians = [];
+const timers = new Map();
 
 console.log("Auditor started...");
 
@@ -46,14 +47,40 @@ s.bind(protocol.PROTOCOL_PORT, function() {
 // function executed each time a datagram is received
 s.on('message', function(msg, source) {
     console.log("Data has arrived: " + msg + ". Source port: " + source.port);
-    //console.log(msg.getData("uuid"));
-    //console.log(msg.getData("sound"));
+    var obj = JSON.parse(msg);
+    if(!musicians.find(({ uuid }) => uuid === obj.uuid)) {
+        console.log("new item");
+        musicians.push({
+            uuid: obj.uuid,
+            sound: obj.sound,
+            activeSince: Date.now()
+        });
+        timers.set(obj.uuid, setTimeout(deleteMusician.bind(null, obj.uuid), 5000));
+    }
+    else {
+        clearTimeout(timers.get(obj.uuid));
+        timers.set(obj.uuid, setTimeout(deleteMusician.bind(obj.uuid), 5000));
+    }
+    console.log(musicians.find(({ uuid }) => uuid === obj.uuid));
+
+});
+
+
+s.on('connection', function() {
+    res.send(sendMusicians());
 });
 
 // fonction à executer à chaque connexion client
 function sendMusicians(){
-    return"coucou";
+    return JSON.stringify(Array.from(musicians.entries()));
 
+}
+
+// fonction à executer lorsqu'un musicien n'a pas émis de son depuis 5 secondes
+function deleteMusician(uuid) {
+    console.log("Deleted entry: " + uuid);
+    delete musicians.find(({ uuid }) => uuid === uuid);
+    //delete musicians[uuid];
 }
 
 
